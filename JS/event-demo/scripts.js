84a6bb709b6c7a3011827d3b8f09c563809870f4
craigkelleher <kelleher.craig@gmail.com>
2022-10-25 11:06:47 -0700

    Demonstrating DOM interactions with event listners

---
+++ JS/event-demo/scripts.js
@@ -0,0 +1,33 @@
+// Initial Test
+//const btn = document.querySelector("#btn")
+//const btn = document.getElementById("myBtn")
+//.addEventListener("click", randBackgrdColor);
+
+//cost btn = document.querySelector("#myBtn");
+//btn.addEventListener("click", () => {
+//    alert("Hi class, cool day")
+//})
+
+//way 3 equivalent
+// const btn = document.querySelector("#btn").addEventListener("click", myFn)
+// function myFn(){     
+//     alert("Hi class, cool day")
+// }
+
+// Alternative, just using variable
+// const btn = btn.addEventListener("click", () => {
+//     alert("Hi class, cool day")
+// })
+
+//Way 3
+// const btn = document.querySelector("#btn").addEventListener("click", () => {
+//     alert("Hi class, cool day right?")
+// })
+
+//way 2
+// const btn = document.querySelector("#btn").onclick = () => {
+//     alert("Hi class, cool day")
+// }
+
+//bad way 1
+//<div id=“myDiv” onclick=“randBackgrdColor(this)”></div>
